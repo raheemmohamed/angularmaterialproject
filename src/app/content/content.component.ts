72a49a0d634f852e8a0649e614f3ec71fb690256
Raheem <mohamedraheem0555@gmail.com>
2018-11-28 22:26:45 +0530

    Expandable Table test

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,11 +1,16 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,ChangeDetectionStrategy } from '@angular/core';
 import {FormControl} from '@angular/forms';
-import {Observable, from} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig} from '@angular/material';
+import { Observable   } from 'rxjs';
+import { of  } from 'rxjs';
+
+
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA, MatDialogConfig, MatSort, MatPaginator, Sort, PageEvent} from '@angular/material';
 import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 
+import { fromMatSort, sortRows } from '../../app/datasource-utils';
+import { fromMatPaginator, paginateRows } from '../../app/datasource-utils';
+import { map } from 'rxjs/internal/operators/map';
 
 export interface DialogData {
   animal: string;
@@ -20,23 +25,39 @@ export interface DialogData {
 export class ContentComponent implements OnInit {
 
   // showFiller = false;
+
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  displayedRows$: Observable<ShipData[]>;
+  totalRows$: Observable<number>;
   
 
  
   constructor(public dialogs: MatDialog , private dialog: MatDialog) { }
 
   ngOnInit() {
-    
+    const sortEvents$: Observable<Sort> = fromMatSort(this.sort);
+    const pageEvents$: Observable<PageEvent> = fromMatPaginator(this.paginator);
+
+    const rows$ = of(exampleShips);
+
+    this.totalRows$ = rows$.pipe(map(rows => rows.length));
+    this.displayedRows$ = rows$.pipe(sortRows(sortEvents$), paginateRows(pageEvents$));
   }
+
+
 
   heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
   number = [];
 
   addHero(newHero: string, newHero2:string) {
-    if (newHero || newHero2) {
+   
       this.heroes.push(newHero);
+    
+    
       this.number.push(newHero2);
-    }
+   
   }
 
   openDialog() {
@@ -52,3 +73,78 @@ export class ContentComponent implements OnInit {
   
 
 }
+export interface ShipData {
+  vesselId: string;
+  vesselName: string;
+  ircs?: string;
+  countryCode: string;
+  vesselStatus: string;
+  grossTonnage?: number;
+  hullNumber?: string;
+  vesselType: string;
+  jonesActEligible: boolean;
+  disabledDate?: string;
+}
+
+export const exampleShips: ShipData[] = [
+  {
+    'ircs': 'V7QV6',
+    'vesselName': 'KYOWA ORCHID',
+    'countryCode': 'US',
+    'vesselId': 'IMO8675368',
+    'vesselStatus': 'S',
+    'grossTonnage': 1,
+    'hullNumber': undefined,
+    'vesselType': 'A21',
+    'jonesActEligible': false,
+    'disabledDate': undefined
+  },
+  {
+    'ircs': 'WECH',
+    'vesselName': 'MANULANI',
+    'countryCode': 'US',
+    'vesselId': 'IMO8675370',
+    'vesselStatus': 'S',
+    'grossTonnage': 1,
+    'hullNumber': undefined,
+    'vesselType': 'A21',
+    'jonesActEligible': false,
+    'disabledDate': undefined
+  },
+  {
+    'ircs': 'WDB5483',
+    'vesselName': 'FREEDOM',
+    'countryCode': 'US',
+    'vesselId': 'IMO8675382',
+    'vesselStatus': 'S',
+    'grossTonnage': 1,
+    'hullNumber': undefined,
+    'vesselType': 'A21',
+    'jonesActEligible': false,
+    'disabledDate': undefined
+  },
+  {
+    'ircs': 'C6VT5',
+    'vesselName': 'CORAL',
+    'countryCode': 'BF',
+    'vesselId': 'IMO8675394',
+    'vesselStatus': 'S',
+    'grossTonnage': 1,
+    'hullNumber': undefined,
+    'vesselType': 'A21',
+    'jonesActEligible': false,
+    'disabledDate': undefined
+  },
+  {
+    'ircs': 'CQOD',
+    'vesselName': 'AUTORACER',
+    'countryCode': 'PO',
+    'vesselId': 'IMO8675409',
+    'vesselStatus': 'S',
+    'grossTonnage': 1,
+    'hullNumber': undefined,
+    'vesselType': 'A21',
+    'jonesActEligible': false,
+    'disabledDate': undefined
+  }
+];
